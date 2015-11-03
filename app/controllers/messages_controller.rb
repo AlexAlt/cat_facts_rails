313bf1ffec320fd3cd31c82ff7adc666a5f09cc3
Leah Klamkin & Alex Altieri <student@epicodus.com>
2015-11-03 10:02:03 -0800

    Fix message form with option to either input a number manually or select an existing contact

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,9 +9,19 @@ class MessagesController < ApplicationController
   end
 
   def create
-    @message = Message.new(message_params)
+    from = params[:from]
+    body = params[:body]
+    if params[:to].present? && params[:to_collection].present?
+      flash[:notice] = "You can only choose one number"
+      render "new"
+    elsif params[:to].present?
+      to = params[:to]
+    else params[:to_collection].present?
+      to = params[:to_collection]
+    end
+    params = { to: to, from: from, body: body}
+    @message = Message.new(params)
     @contacts = Contact.all
-
 
     if @message.save
       flash[:notice] = "Your message was sent!"
@@ -25,9 +35,9 @@ class MessagesController < ApplicationController
     @message = Message.find(params[:id])
   end
 
-private
-
-  def message_params
-    params.require(:message).permit(:to, :from, :body)
-  end
+# private
+#
+#   def message_params
+#     params.require(:message).permit(:to, :from, :body)
+#   end
 end
